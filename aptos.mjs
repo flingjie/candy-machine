318932fee4d115e315a5c0caecacb8851e0fb9ee
flingjie <fanlingjie.cn@gmail.com>
2022-11-01 20:08:35 +0800

    update aptos

--- aptos.mjs
+++ aptos.mjs
@@ -1,4 +1,4 @@
-import { AptosClient, AptosAccount, TokenClient } from "aptos";
+import { AptosClient, AptosAccount, TokenClient,FaucetClient } from "aptos";
 
 const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
 export const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet.aptoslabs.com";
@@ -6,7 +6,7 @@ export const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet
 const client = new AptosClient(NODE_URL);
 const tokenClient = new TokenClient(client);
 const account = AptosAccount.fromDerivePath("m/44'/637'/2'/0'/0'", "");
-const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL); // <:!:section_1a
+const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL); 
 
 console.log(`account: ${account.address()}`);
 const collectionName = "Legend's";
