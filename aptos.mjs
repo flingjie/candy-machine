6fab08c5c2516bd1ec99c3565ca0d238d76181b6
flingjie <fanlingjie.cn@gmail.com>
2022-11-01 20:03:27 +0800

    update aptos

--- aptos.mjs
+++ aptos.mjs
@@ -1,12 +1,21 @@
 import { AptosClient, AptosAccount, TokenClient } from "aptos";
 
-const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.testnet.aptoslabs.com";
+const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
+export const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet.aptoslabs.com";
+
 const client = new AptosClient(NODE_URL);
 const tokenClient = new TokenClient(client);
 const account = AptosAccount.fromDerivePath("m/44'/637'/2'/0'/0'", "");
+const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL); // <:!:section_1a
+
 console.log(`account: ${account.address()}`);
 const collectionName = "Legend's";
 
+(async () => {
+  await faucetClient.fundAccount(account.address(), 100_000_000);
+  console.log(`Account balance: ${await coinClient.checkBalance(account)}`);
+
+})
 export async function mint (item) {
   const txnHash = await tokenClient.createToken(
     account,
