650b8e230f27c8561a89fac44c3b4ab281d98f82
flingjie <fanlingjie.cn@gmail.com>
2022-11-01 19:52:59 +0800

    update aptos

--- aptos.mjs
+++ aptos.mjs
@@ -1,9 +1,9 @@
 import { AptosClient, AptosAccount, TokenClient } from "aptos";
 
-const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
+const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.testnet.aptoslabs.com";
 const client = new AptosClient(NODE_URL);
 const tokenClient = new TokenClient(client);
-const account = AptosAccount.fromDerivePath("", "xxx");
+const account = AptosAccount.fromDerivePath("m/44'/637'/2'/0'/0'", "");
 console.log(`account: ${account.address()}`);
 const collectionName = "Legend's";
 
